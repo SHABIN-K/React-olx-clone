f10fb162dbcaed31d133308264f1adbb040044e7
Shabin-k <helloshabink@gmail.com>
2022-12-07 13:58:31 +0530

    add fuction to sell button and fixed some bugs

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,14 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './Create.css';
 import Header from '../Header/Header';
+import { useContext } from 'react';
+import { AuthContext, FirebaseContext } from '../../Store/Context';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
+  const [name, setName] = useState('')
+  const [category, setCategory] = useState('')
+  const [price, setPrice] = useState('')
+  const [image, setImage] = useState(null)
+
+  const {Firebase} = useContext(FirebaseContext)
+  const {user} = useContext(AuthContext)
+
+  const date = new Date();
+
+  const navigate = useNavigate();
+
+  const handleSubmit =() =>{
+    Firebase.storage().ref(`/image/${image.name}`).put(image).then(({ref})=>{
+      ref.getDownloadURL().then((url)=>{
+        console.log(url)
+        Firebase.firestore().collection("products").add({
+          name,
+          category,
+          price,
+          url,
+          userid:user.uid,
+          createdAt:date.toDateString()
+        })
+        navigate('/');
+      })
+    })
+  }
   return (
     <Fragment>
       <Header />
       <card>
         <div className="centerDiv">
-          <form>
             <label htmlFor="fname">Name</label>
             <br />
             <input
@@ -16,6 +46,8 @@ const Create = () => {
               type="text"
               id="fname"
               name="Name"
+              value={name}
+              onChange={(e)=>setName(e.target.value)}
               defaultValue="John"
             />
             <br />
@@ -26,22 +58,31 @@ const Create = () => {
               type="text"
               id="fname"
               name="category"
+              value={category}
+              onChange={(e)=>setCategory(e.target.value)}
               defaultValue="John"
             />
             <br />
             <label htmlFor="fname">Price</label>
             <br />
-            <input className="input" type="number" id="fname" name="Price" />
-            <br />
-          </form>
+            <input 
+              className="input" 
+              type="number" 
+              id="fname" 
+              name="Price"
+              value={price}
+              onChange={(e)=>setPrice(e.target.value)}
+             />
+             <br />
           <br />
-          <img alt="Posts" width="200px" height="200px" src=""></img>
-          <form>
+          <img alt="Posts" width="200px" height="200px" src={image ? URL.createObjectURL(image) : ''}></img>
             <br />
-            <input type="file" />
+            <input onChange={(e)=>{
+              setImage(e.target.files[0])
+            }} type="file" />
             <br />
-            <button className="uploadBtn">upload and Submit</button>
-          </form>
+            <button className="uploadBtn" 
+              onClick={handleSubmit}>upload and Submit</button>
         </div>
       </card>
     </Fragment>
